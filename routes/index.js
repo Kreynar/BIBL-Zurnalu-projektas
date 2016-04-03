f34a005a70c459eb3423d449bf3d8f4df6d40647
Kreynar <martynasgs@gmail.com>
2016-04-03 21:47:51 +0300

    w/e

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+var mongodb = require('mongodb');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lietuvos mokslo žurnalai' });
+
+  var MongoClient = mongodb.MongoClient;
+  var urlManoSukurtosMongoDuombazes = 'mongodb://localhost:27017/mongoDBZurnaluProjekto';
+  // res.render('index', { title: 'Lietuvos mokslo žurnalai' });
+  MongoClient.connect(urlManoSukurtosMongoDuombazes, function(err, db) {
+    if (err) {
+      console.log('Erroras meginant connectintis prie mongoDBZurnaluProjekto:', err);
+    }
+    else {
+      console.log('Prisijungem prie mongoDBZurnaluProjekto!');
+      var collectionZurnalai = db.collection('zurnalai');
+      collectionZurnalai.find({}).toArray(function(err, masyvasZurnaluDocumentu){
+        if (err) {
+          console.log(err);
+        }
+        // else if (sarasasZurnalu.length) {
+        //
+        // }
+        else {
+          console.log('masyvasZurnaluDocumentu1: ', masyvasZurnaluDocumentu, '@@@');
+          res.render('index', {
+            masyvasZurnaluDocumentu: masyvasZurnaluDocumentu
+          });
+          db.close(function() {
+            console.log('Tiketina, kad ivykdyta db.close()')
+          });
+        }
+      });
+
+    }
+  });
+
+
 });
 
 
@@ -13,7 +47,21 @@ router.get('/prisijungimas', function(req, res, next) {
   res.render('prisijungimas', { title: 'Lietuvos mokslo žurnalai' });
 });
 
-module.exports = router;
+
+
+/* GET Userlist page. */
+router.get('/useriusarasas', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('useriusarasas', {
+      "useriusarasas" : docs
+    });
+  });
+});
+
+
+
 
 
 //9. Open sampsite/routes/index.js in a text editor like NotePad++ and add the following after router = express.Router() but before module.exports = router;
@@ -56,3 +104,7 @@ router.get('/thelist', function(req, res){
     }
   });
 });
+
+
+
+module.exports = router;
