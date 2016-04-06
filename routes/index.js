e7b0a9285934013c37f15470d4d06c7bf178549e
Kreynar <martynasgs@gmail.com>
2016-04-06 18:05:54 +0300

    pridejau bootstrap del pavyzdziu paemimo is jo katalogo in node_modules

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,6 @@ var kiekisStulpeliuRodomu = masyvasDvimatisPavadinimuStulpeliuIrAtitinkamuFieldu
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
   var MongoClient = mongodb.MongoClient;
   // var urlManoSukurtosMongoDuombazes = 'mongodb://localhost:27017/mongoDBZurnaluProjekto';
   var urlManoSukurtosMongoDuombazes = 'mongodb://localhost:27017/mongoDBZurnaluProjekto';
@@ -75,8 +74,6 @@ router.get('/', function(req, res, next) {
 
     }
   });
-
-
 });
 
 
@@ -96,51 +93,6 @@ router.get('/useriusarasas', function(req, res) {
     res.render('useriusarasas', {
       "useriusarasas" : docs
     });
-  });
-});
-
-
-
-
-
-//9. Open sampsite/routes/index.js in a text editor like NotePad++ and add the following after router = express.Router() but before module.exports = router;
-
-router.get('/thelist', function(req, res){
-
-  // Get a Mongo client to work with the Mongo server
-  var MongoClient = mongodb.MongoClient;
-
-  // Define where the MongoDB server is
-  var url = 'mongodb://localhost:27017/sampsite';
-
-  // Connect to the server
-  MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the Server', err);
-    } else {
-      // We are connected
-      console.log('Connection established to', url);
-
-      // Get the documents collection
-      var collection = db.collection('students');
-
-      // Find all students
-      collection.find({}).toArray(function (err, result) {
-        if (err) {
-          res.send(err);
-        } else if (result.length) {
-          res.render('studentlist',{
-
-            // Pass the returned database documents to Jade
-            "studentlist" : result
-          });
-        } else {
-          res.send('No documents found');
-        }
-        //Close connection
-        db.close();
-      });
-    }
   });
 });
 
